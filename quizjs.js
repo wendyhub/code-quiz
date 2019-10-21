999b9668136062755187da87e17d255bd0ee6bc5
wwhub <wendy98289@gmail.com>
2019-10-21 01:05:18 -0400

    fixed spacing on questions

--- quizjs.js
+++ quizjs.js
@@ -1,28 +1,28 @@
 //arrray of the quiz questions, avaialble choices, and correct answers
 var questions = [{
         title: "Which of the following function of Array object adds one or more elements to the front of an array and returns the new length of the array?",
-        choices: ["unshift()", "sort()", "splice()", "toString()"],
-        answer: "unshift()"
+        choices: ["unshift( )", "sort( )", "splice( )", "toString( )"],
+        answer: "unshift( )"
     },
     {
         title: "Which built-in method adds one or more elements to the end of an array and returns the new length of the array?",
-        choices: ["last()", "put()", "push()", "pop()"],
-        answer: "push()"
+        choices: ["last( )", "put( )", "push( )", "pop( )"],
+        answer: "push( )"
     },
     {
         title: " Which built-in method returns the characters in a string beginning at the specified location?",
-        choices: ["substr()", "getSubstring()", "slice()", "None of the above."],
-        answer: "substr()"
+        choices: ["substr( )", "getSubstring( )", "slice( )", "None of the above."],
+        answer: "substr( )"
     },
     {
         title: "Which of the following function of Array object adds and/or removes elements from an array?",
-        choices: ["toSource()", "sort()", "unshift()", "splice()"],
-        answer: "splice()"
+        choices: ["toSource( )", "sort( )", "unshift( )", "splice( )"],
+        answer: "splice( )"
     },
     {
         title: "Which of the following function of String object combines the text of two strings and returns a new string?",
-        choices: ["add()", "concat()", " merge()", "append()"],
-        answer: "concat()"
+        choices: ["add( )", "concat( )", " merge( )", "append( )"],
+        answer: "concat( )"
     }
 ]
 
