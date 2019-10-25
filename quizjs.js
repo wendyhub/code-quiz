7c554b8bbc31f91515af46898e675e8a8f62d2ed
wwhub <wendy98289@gmail.com>
2019-10-24 23:46:46 -0400

    placeholder text for name input

--- quizjs.js
+++ quizjs.js
@@ -59,7 +59,7 @@ function endGame() {
     <h2>Game over!</h2>
     <h3>You got a ` + score +  ` /100!</h3>
     <h3>That means you got ` + score / 20 +  ` questions correct!</h3>
-    <input type="text" id="name" value="What's your name?">
+    <input type="text" id="name" placeholder="First name"> 
     <button onclick="setScore()">Set score!</button>`;
 
     document.getElementById("quizBody").innerHTML = quizContent;
